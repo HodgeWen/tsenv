aaec58da8bb6bcb6ba7b3f1fd944b954dee0c46b
wen <emailwen@qq.com>
2019-04-29 18:07:21 +0800

    update

---
+++ v1/basicTypes/index.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+var m1_1 = require("./m1");
+var a = 1;
+var pow = function (num) { return Promise.resolve(num * num); };
+console.log(a, m1_1.b, pow(2));
---
+++ v1/basicTypes/index.ts
@@ -0,0 +1,6 @@
+import {b} from './m1'
+const a: number = 1
+
+// const pow = (num: number) => Promise.resolve(num * num)
+
+// console.log(a, b, pow(2))
